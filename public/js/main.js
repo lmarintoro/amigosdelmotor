f9f525a1edba1f26d3d629bc24f6f0eff2ed88d5
Luis Guillermo Marin <lmarin@vates.org>
2016-10-31 15:31:15 -0300

    update

--- public/js/main.js
+++ public/js/main.js
@@ -79,7 +79,23 @@ function main() {
 				        [1600, 5]
 				      ],
   	  });
+ $("#emergencia").owlCarousel({
 
+  	      navigation : false, // Show next and prev buttons
+  	      slideSpeed : 300,
+  	      paginationSpeed : 400,
+  	      autoHeight : true,
+  	      itemsCustom : [
+				        [0, 1],
+				        [450, 2],
+				        [600, 2],
+				        [700, 2],
+				        [1000, 4],
+				        [1200, 5],
+				        [1400, 5],
+				        [1600, 5]
+				      ],
+  	  });
       $("#testimonial").owlCarousel({
         navigation : false, // Show next and prev buttons
         slideSpeed : 300,
